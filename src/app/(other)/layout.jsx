6ac323d005d9735b653f4dec22561856e9f1a8ff
johra nayab <johranayab@gmail.com>
2024-11-16 15:28:37 +0530

    just add authentication Using Clerk

--- src/app/(other)/layout.jsx
+++ src/app/(other)/layout.jsx
@@ -2,6 +2,8 @@ import localFont from "next/font/local";
 import ".././globals.css";
 import LeftSidebar from "@/Components/LeftSidebar";
 import RightSidebar from "@/Components/RightSidebar";
+import { ClerkProvider, ClerkLoaded, ClerkLoading } from "@clerk/nextjs";
+import Loader from "@/Components/Loader";
 
 const geistSans = localFont({
   src: ".././fonts/GeistVF.woff",
@@ -21,20 +23,29 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <div className="flex justify-between max-w-6xl mx-auto">
-          <div className="hidden sm:inline border-r h-screen sticky top-0 "  >
-            <LeftSidebar />
+    <ClerkProvider>
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          <ClerkLoading>
+            <Loader />
+          </ClerkLoading>
+          <ClerkLoaded>
+
+          
+          <div className="flex justify-between max-w-6xl mx-auto">
+            <div className="hidden sm:inline border-r h-screen sticky top-0 ">
+              <LeftSidebar />
+            </div>
+            <div className="w-2xl flex-1 "> {children}</div>
+            <div className="lg:flex-col p-3 h-screen border-l  hidden lg:flex w-[24rem] ">
+              <RightSidebar />
+            </div>
           </div>
-          <div className="w-2xl flex-1 "> {children}</div>
-          <div className="lg:flex-col p-3 h-screen border-l  hidden lg:flex w-[24rem] "> 
-            <RightSidebar />
-          </div>
-        </div>
-      </body>
-    </html>
+          </ClerkLoaded>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
